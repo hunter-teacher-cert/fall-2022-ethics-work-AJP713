05b2e78da4858a3feb87ad10f3aed01a3eb00033
Adam Prado <64868729+AJP713@users.noreply.github.com>
2022-10-05 18:15:38 +0000

    update name types

--- week04/regex.py
+++ week04/regex.py
@@ -2,12 +2,16 @@ import re
 
 
 def find_date(line):
-    pattern = "((Mr.|Mrs.|Ms.|Dr.)\s[A-Z]\w*)" #looks for prefix with a capital letter and names
+    pattern = "((Mr.|Mrs.|Ms.|Dr.)\s[A-Z]\w*)" #looks for prefix with a capital letter and names  ex:  Mr. Prado
     result = re.findall(pattern,line)
 
-    pattern=r'([A-Z]\w*\s[A-z]\w*)' #looks for two consecutive words starting with capital letters.
+    pattern=r'([A-Z]\w*\s[A-Z]\w*)' #looks for two consecutive words starting with capital letters. ex: Adam Prado
     result = result + re.findall(pattern,line)
-    pattern=r'([A-Z]\.\s[A-Z]\w*)' #looks for Initial and capitalized last name
+  
+    pattern=r'([A-Z]\.\s[A-Z]\w*)' #looks for Initial and capitalized last name ex: A. Prado
+    result = result + re.findall(pattern,line)
+
+    pattern=r'([A-Z]\w*\s[A-Z]\.\s[A-Z]\w*)' #looks for Name then Initial and capitalized last name ex: Adam J. Prado
     result = result + re.findall(pattern,line)
   
     return result
